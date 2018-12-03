270489499f520aa2bb8ce3603c14ae0d0e1ce887
Scott Downie <scott.downie@tag-games.com>
2018-12-03 09:55:15 +0000

    Refactoring day 1 solution to use built-in sum

--- 2018/Day1/main.py
+++ 2018/Day1/main.py
@@ -1,19 +1,14 @@
 # Part One - Simple addition puzzle, reading a bunch of integer input
 # and keeping track of the count
 #
-def part_one():
-	total = 0
-	with open('input.txt') as frequencies:
-		for num, line in enumerate(frequencies):
-			freq = int(line)
-			total += freq
-	return total
+def part_one(frequencies):
+	return sum(frequencies)
 
 # Part Two - Loop through doing addition until the same number if reached twice. 
 # Returns the first number reached twice.
 # NOTE: May need to loop multiple times
 #
-def part_two():
+def part_two(frequencies):
 	seen_totals = set()
 	total = 0
 	counter = 0
@@ -21,14 +16,15 @@ def part_two():
 	#Starting value has been seen
 	seen_totals.add(total)
 
-	with open('input.txt') as frequencies:
-		as_list = [int(line) for line in frequencies]
-		while True:
-			total += as_list[counter % len(as_list)]
-			if total in seen_totals:
-				return total
-			seen_totals.add(total)
-			counter = counter + 1
+	while True:
+		total += frequencies[counter % len(as_list)]
+		if total in seen_totals:
+			return total
+		seen_totals.add(total)
+		counter = counter + 1
 
-print(part_one())
-print(part_two())
+with open('input.txt') as frequencies:
+	as_list = [int(line) for line in frequencies]
+
+	print(part_one(as_list))
+	print(part_two(as_list))
