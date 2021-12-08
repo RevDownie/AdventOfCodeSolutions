e684d48c1290cf1812ce90e8091fdf4fc9e1e68e
Scott Downie <scott.downie@live.com>
2021-12-08 13:03:59 +0000

    2021 Day 7 Part 2

--- 2021/Day7/day7.zig
+++ 2021/Day7/day7.zig
@@ -30,20 +30,45 @@ pub fn main() !void {
     }
     std.sort.sort(u32, vals.items, {}, comptime std.sort.asc(u32));
 
-    const result_1 = shortest_moves(vals.items);
-    const result_2 = 0;
+    const result_1 = shortest_moves_1_fuel_per_move(vals.items);
+    const result_2 = shortest_moves_gauss_fuel_per_move(vals.items);
     try stdout.print("Part 1: {}, Part 2: {} ms: {}\n", .{ result_1, result_2, @intToFloat(f64, t.read()) / 1000000.0 });
 }
 
-fn shortest_moves(vals: []u32) !u32 {
+fn shortest_moves_1_fuel_per_move(vals: []u32) u32 {
     //Find the median and then sum the distance between each value and the median
     const median = vals[vals.len / 2];
 
-    var sum: u32 = 0;
+    var moves_sum: u32 = 0;
     for (vals) |v| {
         const d = if (v > median) v - median else median - v;
-        sum += d;
+        moves_sum += d;
     }
 
-    return sum;
+    return moves_sum;
+}
+
+/// The mean is within 0.5 of the actual answer so need to check floor and ceil and pick the lowest
+fn shortest_moves_gauss_fuel_per_move(vals: []u32) u32 {
+    var sum: u32 = 0;
+    for (vals) |v| {
+        sum += v;
+    }
+
+    const count = @intToFloat(f32, vals.len);
+    const mean = @intToFloat(f32, sum) / count;
+    const mean_f = @floatToInt(u32, @floor(mean));
+    const mean_c = @floatToInt(u32, @ceil(mean));
+
+    var moves_sum_f: u32 = 0;
+    var moves_sum_c: u32 = 0;
+    for (vals) |v| {
+        const d_f = if (v > mean_f) v - mean_f else mean_f - v;
+        moves_sum_f += (d_f * (d_f + 1)) / 2; //Triangular or Gauss sum 0, 1, 3, 6, 10
+
+        const d_c = if (v > mean_c) v - mean_c else mean_c - v;
+        moves_sum_c += (d_c * (d_c + 1)) / 2;
+    }
+
+    return std.math.min(moves_sum_c, moves_sum_f);
 }
