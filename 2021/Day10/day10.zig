14fb1f3532261ba895770a019a56fdcbc818521b
Scott Downie <scott.downie@live.com>
2021-12-20 11:02:10 +0000

    2021 Day 10 Part 2

--- 2021/Day10/day10.zig
+++ 2021/Day10/day10.zig
@@ -7,6 +7,7 @@ const bracket_stack_max_size = 150;
 /// Advent of code - Day 10
 ///
 /// Part 1 - Find mismatched closing bracket types
+/// Part 2 - Finish incomplete bracket sequences
 ///
 pub fn main() !void {
     const input_file = try fs.cwd().openFile("input.txt", .{});
@@ -21,7 +22,7 @@ pub fn main() !void {
     const t = try timer.start();
 
     const result_1 = calculate_corruption_score(input_buffer[0..input_len]);
-    const result_2 = 0;
+    const result_2 = calculate_completion_score(input_buffer[0..input_len]);
     try stdout.print("Part 1: {}, Part 2: {} ms: {}\n", .{ result_1, result_2, @intToFloat(f64, t.read()) / 1000000.0 });
 }
 
@@ -30,9 +31,7 @@ pub fn main() !void {
 /// of the stack then it's corrupt
 ///
 fn calculate_corruption_score(input_buffer: []u8) !u32 {
-    const BracketStack = std.SinglyLinkedList(u8);
-    var bracket_stack = BracketStack{};
-    var bracket_stack_node_buffer: [bracket_stack_max_size]BracketStack.Node = undefined;
+    var bracket_stack: [bracket_stack_max_size]u8 = undefined;
     var node_head: usize = 0;
 
     var score: u32 = 0;
@@ -43,18 +42,19 @@ fn calculate_corruption_score(input_buffer: []u8) !u32 {
         if (c == '\n') {
             //Next chunk
             node_head = 0;
-            bracket_stack = BracketStack{};
             continue;
         }
 
         if (is_open_bracket(c)) {
-            bracket_stack_node_buffer[node_head] = BracketStack.Node{ .data = c };
-            bracket_stack.prepend(&bracket_stack_node_buffer[node_head]);
+            //Push
+            bracket_stack[node_head] = c;
             node_head += 1;
         } else {
-            const open_bracket = bracket_stack.popFirst().?.data;
-            if (get_close(open_bracket) != c) {
-                score += get_score(c);
+            //Pop
+            node_head -= 1;
+            const open_bracket = bracket_stack[node_head];
+            if (get_closing_bracket(open_bracket) != c) {
+                score += get_corruption_score(c);
                 //Skip to the end of the chunk as we are only concerned with the first mismatch on each line
                 while (input_buffer[i] != '\n') : (i += 1) {}
                 i -= 1; //Stop of the \n so we reset next time round the loop
@@ -65,6 +65,54 @@ fn calculate_corruption_score(input_buffer: []u8) !u32 {
     return score;
 }
 
+/// Discard corrupt lines and for any incomplete lines complete the bracket sequence and calculate the score by finding the median
+///
+fn calculate_completion_score(input_buffer: []u8) !u64 {
+    var bracket_stack: [bracket_stack_max_size]u8 = undefined;
+    var node_head: usize = 0;
+
+    var chunk_scores = std.ArrayList(u64).init(std.heap.page_allocator);
+    defer chunk_scores.deinit();
+
+    var i: usize = 0;
+    while (i < input_buffer.len) : (i += 1) {
+        const c = input_buffer[i];
+        if (c == '\n') {
+            //Next chunk - calculate the completion score from any unclosed braces
+            var chunk_score: u64 = 0;
+            while (node_head > 0) : (node_head -= 1) {
+                const open_bracket = bracket_stack[node_head - 1];
+                chunk_score = chunk_score * 5 + get_completion_score(open_bracket);
+            }
+
+            if (chunk_score > 0) {
+                try chunk_scores.append(chunk_score);
+            }
+
+            continue;
+        }
+
+        if (is_open_bracket(c)) {
+            bracket_stack[node_head] = c;
+            node_head += 1;
+        } else {
+            node_head -= 1;
+            const open_bracket = bracket_stack[node_head];
+            if (get_closing_bracket(open_bracket) != c) {
+                //Clear stack and skip to the end of the chunk as we are discarding corrupted lines
+                node_head = 0;
+                while (input_buffer[i] != '\n') : (i += 1) {}
+                i -= 1; //Stop of the \n so we reset next time round the loop
+            }
+        }
+    }
+
+    //Find the median
+    var scores = chunk_scores.items;
+    std.sort.sort(u64, scores, {}, comptime std.sort.asc(u64));
+    return scores[scores.len / 2];
+}
+
 fn is_open_bracket(c: u8) bool {
     return switch (c) {
         '{', '[', '(', '<' => true,
@@ -72,7 +120,7 @@ fn is_open_bracket(c: u8) bool {
     };
 }
 
-fn get_close(c: u8) u8 {
+fn get_closing_bracket(c: u8) u8 {
     return switch (c) {
         '(' => ')',
         '[' => ']',
@@ -82,12 +130,22 @@ fn get_close(c: u8) u8 {
     };
 }
 
-fn get_score(c: u8) u32 {
+fn get_corruption_score(c: u8) u32 {
     return switch (c) {
         ')' => 3,
         ']' => 57,
         '}' => 1197,
         '>' => 25137,
+        else => unreachable,
+    };
+}
+
+fn get_completion_score(c: u8) u32 {
+    return switch (c) {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
         else => unreachable,
     };
 }
