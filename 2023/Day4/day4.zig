f29238276cc750adb2bfeb7bebc9f113617a549d
Scott Downie <scott.downie@scopely.com>
2023-12-04 09:07:47 +0000

    2023 Day 4 Part 2

--- 2023/Day4/day4.zig
+++ 2023/Day4/day4.zig
@@ -2,30 +2,38 @@ const std = @import("std");
 
 const input_file = @embedFile("input.txt");
 
+const Result = struct {
+    part1: u32,
+    part2: u32,
+};
+
 /// Advent of code - Day 4
 ///
 /// Part 1 - Count number of matches in both lists and double score for each match
-/// Part 2 - ???
+/// Part 2 - Madness! Count the number of originals and copies of cards
 ///
 pub fn main() !void {
     const timer = std.time.Timer;
 
-    var buffer: [1024 * 20]u8 = undefined;
+    var buffer: [1024 * 30]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
 
     var t = try timer.start();
 
-    const result_1 = try part1(input_file[0..], fba.allocator());
-    const result_2 = 0;
+    const result = try run(input_file[0..], fba.allocator());
     const duration: f64 = @floatFromInt(t.read());
-    std.debug.print("Part 1: {}, Part 2: {} ms: {d:.5}\n", .{ result_1, result_2, duration / 1000000.0 });
+    std.debug.print("Part 1: {}, Part 2: {} ms: {d:.5}\n", .{ result.part1, result.part2, duration / 1000000.0 });
 }
 
-/// Read the numbers from one list and compare the numbers from the other list
+/// Part1: Read the numbers from one list and compare the numbers from the other list
 /// the first match is worth 1 point then doubled for each match. Sum the total score
 ///
-fn part1(data: []const u8, allocator: std.mem.Allocator) !u32 {
-    var total: u32 = 0;
+/// Part2: So each winning number wins a copy of the cards below. So if we have 4 matches in card 1 we win a copy of card 2,3,4,5
+/// when you win on card 2 all copies of card 2 win. We have to count the total number of cards at the end
+///
+fn run(data: []const u8, allocator: std.mem.Allocator) !Result {
+    var card_counts = [_]u32{0} ** 200;
+    var score: u32 = 0;
 
     var line_it = std.mem.tokenize(u8, data, "\n");
     while (line_it.next()) |line| {
@@ -33,7 +41,9 @@ fn part1(data: []const u8, allocator: std.mem.Allocator) !u32 {
 
         var card_it = std.mem.tokenize(u8, line, " ");
         _ = card_it.next(); //Skip the card label
-        _ = card_it.next();
+        const unparsed_card_num = card_it.next() orelse @panic("No card num");
+        const card_idx = try std.fmt.parseInt(u32, unparsed_card_num[0 .. unparsed_card_num.len - 1], 10) - 1;
+        card_counts[card_idx] += 1; // Add the original card
 
         //Winning numbers
         while (card_it.next()) |unparsed| {
@@ -53,12 +63,24 @@ fn part1(data: []const u8, allocator: std.mem.Allocator) !u32 {
             }
         }
 
-        //Use the geometric sequence equation 1,2,4,8,16
-        if (num_matches > 0) {
-            total += std.math.pow(u32, 2, (num_matches - 1));
-        }
         winning_set.deinit();
+
+        //Count score - use the geometric sequence equation 1,2,4,8,16
+        if (num_matches > 0) {
+            score += std.math.pow(u32, 2, (num_matches - 1));
+        }
+
+        //Make copies for part 2
+        var i: usize = 0;
+        while (i < num_matches) : (i += 1) {
+            card_counts[card_idx + i + 1] += 1 * card_counts[card_idx];
+        }
     }
 
-    return total;
+    var total_cards: u32 = 0;
+    for (card_counts) |x| {
+        total_cards += x;
+    }
+
+    return Result{ .part1 = score, .part2 = total_cards };
 }
