119bf0a9799c1af0cd4c4cb41cc3ff483ab9dfc0
Scott Downie <scott.downie@scopely.com>
2023-12-05 11:51:54 +0000

    2023 Day 5 Part 2 (Brute force)
    Tidying up part 1 to allow for easier part 2. This works but takes ages so now going to look to reduce the time

--- 2023/Day5/day5.zig
+++ 2023/Day5/day5.zig
@@ -2,11 +2,23 @@ const std = @import("std");
 
 const input_file = @embedFile("input.txt");
 
+const Seed = struct {
+    start: u64,
+    end: u64,
+};
+
+const Seeds = struct {
+    ranges: [20]Seed,
+    count: usize,
+};
+
 const Map = struct {
     dst_start: u64,
     src_start: u64,
     range: u64,
 };
+
+const Maps = [8]std.ArrayList(Map);
 
 /// Advent of code - Day 5
 ///
@@ -21,10 +33,75 @@ pub fn main() !void {
     var buffer: [1024 * 30]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
 
-    const result_1 = try part1(input_file[0..], fba.allocator());
-    const result_2 = 0;
+    const seeds_1 = try part1ParseSeeds(input_file[0..]);
+    const seeds_2 = try part2ParseSeeds(input_file[0..]);
+    var maps = try parseMaps(input_file[0..], fba.allocator());
+
+    const result_1 = run(seeds_1, maps);
+    const result_2 = run(seeds_2, maps);
+
     const duration: f64 = @floatFromInt(t.read());
     std.debug.print("Part 1: {}, Part 2: {} ms: {d:.5}\n", .{ result_1, result_2, duration / 1000000.0 });
+}
+
+/// Seeds are just indices
+///
+fn part1ParseSeeds(data: []const u8) !Seeds {
+    var seeds = Seeds{ .ranges = undefined, .count = 0 };
+
+    var line_it = std.mem.tokenize(u8, data, "\n");
+
+    if (line_it.next()) |line| {
+        var seed_it = std.mem.tokenize(u8, line, " ");
+        _ = seed_it.next(); //Skip "seeds:"
+        while (seed_it.next()) |seed| {
+            const seed_id = try std.fmt.parseInt(u64, seed, 10);
+            seeds.ranges[seeds.count] = Seed{ .start = seed_id, .end = seed_id + 1 };
+            seeds.count += 1;
+        }
+    }
+
+    return seeds;
+}
+
+/// Seeds are pairs of index and range
+///
+fn part2ParseSeeds(data: []const u8) !Seeds {
+    var seeds = Seeds{ .ranges = undefined, .count = 0 };
+
+    var line_it = std.mem.tokenize(u8, data, "\n");
+
+    if (line_it.next()) |line| {
+        var seed_it = std.mem.tokenize(u8, line, " ");
+        _ = seed_it.next(); //Skip "seeds:"
+        while (seed_it.next()) |seed| {
+            const seed_start = try std.fmt.parseInt(u64, seed, 10);
+            const seed_range = try std.fmt.parseInt(u64, seed_it.next() orelse "", 10);
+            seeds.ranges[seeds.count] = Seed{ .start = seed_start, .end = seed_start + seed_range };
+            seeds.count += 1;
+        }
+    }
+
+    return seeds;
+}
+
+fn parseMaps(data: []const u8, allocator: std.mem.Allocator) !Maps {
+    var maps = [_]std.ArrayList(Map){std.ArrayList(Map).init(allocator)} ** 8;
+    var count: u32 = 0;
+
+    var line_it = std.mem.tokenize(u8, data, "\n");
+    _ = line_it.next(); //Skip "seeds:"
+
+    while (line_it.next()) |line| {
+        if (isDigit(line[0]) == false) {
+            count += 1;
+            continue; //Skip the title but move to the next map
+        }
+
+        var map_it = std.mem.tokenize(u8, line, " ");
+        try maps[count].append(Map{ .dst_start = try std.fmt.parseInt(u64, map_it.next() orelse "", 10), .src_start = try std.fmt.parseInt(u64, map_it.next() orelse "", 10), .range = try std.fmt.parseInt(u64, map_it.next() orelse "", 10) });
+    }
+    return maps;
 }
 
 /// There is a mapping function to convert seeds to soil, soil to fertilizer until we get to location
@@ -32,47 +109,20 @@ pub fn main() !void {
 ///
 /// The maps are sequential so it goes seeds-soil, soil-fertlizer, etc in the data
 ///
-fn part1(data: []const u8, allocator: std.mem.Allocator) !u64 {
-    var line_it = std.mem.tokenize(u8, data, "\n");
-
-    //Seeds
-    var seeds: [20]u64 = undefined;
-    var num_seeds: usize = 0;
-
-    if (line_it.next()) |line| {
-        var seed_it = std.mem.tokenize(u8, line, " ");
-        _ = seed_it.next(); //Skip "seeds:"
-        while (seed_it.next()) |seed| {
-            seeds[num_seeds] = try std.fmt.parseInt(u64, seed, 10);
-            num_seeds += 1;
-        }
-    }
-
-    //Maps
-    var maps = [_]std.ArrayList(Map){std.ArrayList(Map).init(allocator)} ** 8;
-    var num_maps: usize = 0;
-
-    while (line_it.next()) |line| {
-        if (isDigit(line[0]) == false) {
-            num_maps += 1;
-            continue; //Skip the title but move to the next map
-        }
-
-        var map_it = std.mem.tokenize(u8, line, " ");
-        try maps[num_maps].append(Map{ .dst_start = try std.fmt.parseInt(u64, map_it.next() orelse "", 10), .src_start = try std.fmt.parseInt(u64, map_it.next() orelse "", 10), .range = try std.fmt.parseInt(u64, map_it.next() orelse "", 10) });
-    }
-
-    //Run
+fn run(seeds: Seeds, maps: Maps) u64 {
     var i: usize = 0;
     var min_loc: u64 = std.math.maxInt(u64);
-    while (i < num_seeds) : (i += 1) {
-        var input = seeds[i];
-        for (maps) |map| {
-            input = performMap(input, map.items);
-        }
+    while (i < seeds.count) : (i += 1) {
+        var seed = seeds.ranges[i].start;
+        while (seed < seeds.ranges[i].end) : (seed += 1) {
+            var input = seed;
+            for (maps) |map| {
+                input = performMap(input, map.items);
+            }
 
-        if (input < min_loc) {
-            min_loc = input;
+            if (input < min_loc) {
+                min_loc = input;
+            }
         }
     }
 
