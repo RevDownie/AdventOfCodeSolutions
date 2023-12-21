6bc8950f93d7b60088587809ba752a5640b74a01
Scott Downie <scott.downie@scopely.com>
2023-12-21 10:27:03 +0000

    2023 Day 20 Part 2

--- 2023/day_20/src/main.rs
+++ 2023/day_20/src/main.rs
@@ -9,22 +9,20 @@ struct Graph {
     input_idx_map: Vec<Vec<u8>>,
     states: Vec<bool>,
     pulses_from: Vec<u64>,
-    names: Vec<String>,
 }
 
 /// Advent of code - Day 20
 ///
 /// Part 1 - Graph of pulses
-/// Part 2 - ???
+/// Part 2 - Part 2 find the lowest number of button presses that would trigger RX
 ///
 fn main() {
     let now = std::time::Instant::now();
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let (mut graph, start_idx) = parse_graph(input.lines());
+    let (mut graph, start_idx, rx_idx) = parse_graph(input.lines());
 
-    let result_1 = run(&mut graph, start_idx, 1000);
-    let result_2 = 0;
+    let (result_1, result_2) = run(&mut graph, start_idx, rx_idx, 1000);
 
     println!(
         "Part 1: {}, Part 2: {}, took {:#?}",
@@ -36,17 +34,17 @@ fn main() {
 
 /// Parse into a graph of adjacency lists
 ///
-fn parse_graph(lines: std::str::Lines) -> (Graph, usize) {
+fn parse_graph(lines: std::str::Lines) -> (Graph, usize, usize) {
     let mut graph = Graph {
         types: vec!['0'; MAX_NODES],
         output_idx_map: vec![Vec::with_capacity(8); MAX_NODES],
         input_idx_map: vec![Vec::with_capacity(8); MAX_NODES],
         states: vec![false; MAX_NODES],
         pulses_from: vec![0; MAX_NODES],
-        names: vec!["".to_string(); MAX_NODES],
     };
 
     let mut start_idx = 0;
+    let mut rx_idx = 0;
     let mut idx_map = HashMap::new();
     let mut next_idx = 0;
 
@@ -57,7 +55,6 @@ fn parse_graph(lines: std::str::Lines) -> (Graph, usize) {
         let (idx, next) = get_or_insert(&id[1..], next_idx, &mut idx_map);
         next_idx = next;
 
-        graph.names[idx as usize] = id[1..].to_string();
         graph.types[idx as usize] = id.as_bytes()[0] as char;
 
         for a in adjs {
@@ -65,6 +62,10 @@ fn parse_graph(lines: std::str::Lines) -> (Graph, usize) {
             next_idx = next;
             graph.output_idx_map[idx as usize].push(a_idx);
             graph.input_idx_map[a_idx as usize].push(idx);
+
+            if a == "rx" {
+                rx_idx = a_idx as usize;
+            }
         }
 
         if graph.types[idx as usize] == 'b' {
@@ -72,19 +73,32 @@ fn parse_graph(lines: std::str::Lines) -> (Graph, usize) {
         }
     }
 
-    (graph, start_idx)
+    (graph, start_idx, rx_idx)
 }
 
 /// Starting at the start index (Broadcaster), run the simulation
 /// the given number of times.
-/// Count the number of low and high pulses and multiply together
+/// Part 1: Count the number of low and high pulses in the give number of runs and multiply together
+/// Part 2: Use LCM and cyclic nature to find the min number of runs for rx to be triggered
 ///
-fn run(graph: &mut Graph, start_idx: usize, n_runs: usize) -> usize {
+fn run(graph: &mut Graph, start_idx: usize, rx_idx: usize, n_runs: usize) -> (usize, usize) {
     let mut pulses_to_process = VecDeque::new();
     let mut num_low = 0;
     let mut num_high = 0;
+    let mut runs = 0;
+    let mut part_1 = 0;
 
-    for _ in 0..n_runs {
+    //Find all connections to single conjunction model connecting to RX.
+    //Find how many button presses it takes for each of them to send a high pulse which would then cause
+    //it to send a low pulse to RX. Take the LCM
+    let rx_feed_idx = graph.input_idx_map[rx_idx][0];
+    let mut button_presses = vec![0; graph.input_idx_map[rx_feed_idx as usize].len()];
+
+    loop {
+        if runs == n_runs {
+            part_1 = num_low * num_high;
+        }
+
         pulses_to_process.push_back((usize::MAX, start_idx, 0));
         num_low += 1;
 
@@ -97,23 +111,29 @@ fn run(graph: &mut Graph, start_idx: usize, n_runs: usize) -> usize {
             };
 
             if let Some(np) = next_pulse {
-                for i in graph.output_idx_map[t].iter() {
-                    println!(
-                        "SEND -> F: {}, T: {}, P: {}",
-                        if t <= 64 { &graph.names[t] } else { "Button" },
-                        graph.names[*i as usize],
-                        if np == 1 { "H" } else { "L" }
-                    );
+                //Are we sending a high pulse to the rx feeder?
+                for (i, x) in graph.input_idx_map[rx_feed_idx as usize].iter().enumerate() {
+                    if p == 1 && t == (rx_feed_idx as usize) && f == (*x as usize) {
+                        button_presses[i] = runs + 1;
+                        continue;
+                    }
+                }
 
-                    pulses_to_process.push_back((t, *i as usize, np));
+                for nt in &graph.output_idx_map[t] {
+                    pulses_to_process.push_back((t, *nt as usize, np));
                     num_low += 1 - (np as usize);
                     num_high += np as usize;
                 }
             }
         }
-    }
 
-    num_low * num_high
+        if button_presses.iter().all(|&x| x > 0) {
+            let part_2 = button_presses.iter().fold(1, |acc, n| lcm(acc, *n));
+            return (part_1, part_2);
+        }
+
+        runs += 1;
+    }
 }
 
 /// Emits a low pulse to all connected nodes
@@ -161,5 +181,22 @@ fn get_or_insert(id: &str, next_idx: u8, idx_map: &mut HashMap<String, u8>) -> (
             idx_map.insert(id.to_string(), new_idx);
             (new_idx, next_idx + 1)
         }
+    }
+}
+
+/// Find the lowest common multiple of 2 numbers
+///
+fn lcm(a: usize, b: usize) -> usize {
+    let mut max = a.max(b);
+    let mut min = a.min(b);
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return (a * b) / min;
+        }
+
+        max = min;
+        min = res;
     }
 }
